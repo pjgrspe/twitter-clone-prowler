e3095797a451af9febe71bdede108d627e58a95c
Jason Gorospe <8.pjgorospe@gmail.com>
2023-10-29 15:53:57 +0800

    User Profile updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,21 @@ import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
 import LoginModal from '@/components/modals/LoginModal'
 import RegisterModal from '@/components/modals/RegisterModal'
+import { Toaster } from 'react-hot-toast'
+import { SessionProvider } from 'next-auth/react'
+import EditModal from '@/components/modals/EditModal'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <>
+  <SessionProvider session={pageProps.session}>
+    <Toaster />
     <LoginModal />
     <RegisterModal />
+    <EditModal />
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  </>
+  </ SessionProvider>
   
   )
 }
